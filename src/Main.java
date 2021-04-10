151d7eebf0a9ec1af913ff75daf19d55395779f6
colne5u <clement.colne@outlook.com>
2021-04-10 18:26:12 +0200

    Récupération des arguments du programme

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        if(args.length < 3) {
+            System.err.println("Erreur au lancement du programme\n" +
+                    "Veuillez respecter la structure \"java filtreAntiSpam <chemin de la base de tests> <nombre de spam pour les tests> <nombre de ham pour les tests>\"");
+            System.exit(1);
+        }
+        // on récupère le chemin de la base de tests
+        String baseTestPath = args[0];
+        // on récupère le nombre de spams utilisés dans les tests
+        int nbSpamTests = Integer.parseInt(args[1]);
+        // on récupère le nombre de hams utilisés dans les tests
+        int nbHamTests = Integer.parseInt(args[2]);
+
         // On demande à l'utilisateur de rentrer les valeurs pour m_spam et m_ham
         Scanner scan = new Scanner(System.in);
-        System.out.print("Veuillez entrer le nombre de messages spam utilisés pour l'apprentissage (m spam)\n");
+        System.out.print("Combien de SPAM dans la base d’apprentissage ?\n");
         int mspam = scan.nextInt();
-        System.out.print("Veuillez entrer le nombre de messages ham utilisés pour l'apprentissage (m ham)\n");
+        System.out.print("Combien de HAM dans la base d’apprentissage ?\n");
         int mham = scan.nextInt();
 
         Dictionnaire dico = charger_dictionnaire();
