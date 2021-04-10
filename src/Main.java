a02cfa3248faff53d0b3d179e715c3ab16747247
colne5u <clement.colne@outlook.com>
2021-04-10 18:05:05 +0200

    Récupération en entrée utilisateur dans le main de mspam et mham

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // On demande à l'utilisateur de rentrer les valeurs pour m_spam et m_ham
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Veuillez entrer le nombre de messages spam utilisés pour l'apprentissage (m spam)\n");
+        int mspam = scan.nextInt();
+        System.out.print("Veuillez entrer le nombre de messages ham utilisés pour l'apprentissage (m ham)\n");
+        int mham = scan.nextInt();
+
         Dictionnaire dico = charger_dictionnaire();
 
         Message mess = lire_message(dico);
